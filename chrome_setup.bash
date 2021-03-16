6ba20e6a3f64ed57de2c13d29149baebcf839714
Alex Parsons <alex@alexparsons.co.uk>
2021-03-16 12:41:11 +0000

    All chrome setup into one script

--- chrome_setup.bash
+++ chrome_setup.bash
@@ -1,5 +1,19 @@
 #!/bin/bash 
 
+curl -s https://deb.nodesource.com/gpgkey/nodesource.gpg.key | apt-key add - \
+      && echo 'deb https://deb.nodesource.com/node_14.x buster main' > /etc/apt/sources.list.d/nodesource.list
+
+curl -s https://deb.nodesource.com/gpgkey/nodesource.gpg.key | apt-key add - \
+      && echo 'deb https://deb.nodesource.com/node_14.x buster main' > /etc/apt/sources.list.d/nodesource.list
+
+curl -s https://dl.google.com/linux/linux_signing_key.pub | apt-key add - \
+      && echo 'deb http://dl.google.com/linux/chrome/deb/ stable main' > /etc/apt/sources.list.d/google-chrome.list
+
+apt-get -qq update \
+      && apt-get -qq install \
+            google-chrome-stable \
+      && rm -rf /var/lib/apt/lists/*
+
 VERSION=$( curl -s https://chromedriver.storage.googleapis.com/LATEST_RELEASE_$( google-chrome --version | awk '{print $NF}' | cut -d. -f1,2,3 ) )
 wget -q https://chromedriver.storage.googleapis.com/${VERSION}/chromedriver_linux64.zip
 unzip chromedriver_linux64.zip
