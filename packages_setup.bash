2fe01bee6c1e0744726fed4cae899373abafb065
Alex Parsons <alex@alexparsons.co.uk>
2021-06-30 13:32:40 +0000

    added pandoc to common functions

--- packages_setup.bash
+++ packages_setup.bash
@@ -21,3 +21,6 @@ mv chromedriver /usr/bin/chromedriver
 chown root:root /usr/bin/chromedriver
 chmod +x /usr/bin/chromedriver
 rm -f chromedriver_linux64.zip
+
+apt-get update -y
+apt-get install pandoc -y
