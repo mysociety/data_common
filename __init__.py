907add3ebc85e6bcf784548643e5230318e9f2c9
Alex Parsons <alex@alexparsons.co.uk>
2021-09-13 16:37:03 +0000

    Changed default sigfig

--- __init__.py
+++ __init__.py
@@ -5,7 +5,7 @@ functions to speed up and pretify notebooks
 from .exporters import render_to_markdown, render_to_html
 from .charting import (altair_theme, Chart)
 from .progress import Progress, track_progress
-from .helpers.pipe import Pipe, iter_format
+from .helpers.pipe import Pipe, Pipeline, iter_format
 from typing import Union, Optional, List, Callable
 from functools import partial
 
@@ -21,6 +21,7 @@ import builtins as __builtin__
 builtin_print = __builtin__.print
 
 pd.options.mode.chained_assignment = None
+pd.set_option("display.precision", 2)
 
 image_dir = Path("_images")
 render_dir = Path("_render")
