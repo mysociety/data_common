41f9f288d64db49e569864fb7bbd7d0fee9383e5
Alex Parsons <alex@alexparsons.co.uk>
2021-12-16 15:25:47 +0000

    added subtitle font

--- charting/theme.py
+++ charting/theme.py
@@ -137,7 +137,13 @@ font = "Source Sans Pro"
 mysoc_theme = {
     "config": {
         "padding": {"left": 5, "top": 5, "right": 20, "bottom": 5},
-        "title": {"font": font, "fontSize": 30, "anchor": "start"},
+        "title": {
+            "font": font,
+            "fontSize": 30,
+            "anchor": "start",
+            "subtitleFontSize": 20,
+            "subtitleFont": "Source Sans Pro",
+        },
         "axis": {
             "labelFont": font,
             "labelFontSize": 14,
