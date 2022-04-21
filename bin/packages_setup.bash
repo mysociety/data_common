456a6a28bbb0c6acda5babda7f831161a7ba7f4b
Alex Parsons <alex.parsons@mysociety.org>
2022-04-21 16:47:53 +0100

    Don't use virtualenvs

--- bin/packages_setup.bash
+++ bin/packages_setup.bash
@@ -2,6 +2,9 @@
 
 curl -sSL https://raw.githubusercontent.com/python-poetry/poetry/master/get-poetry.py | python -
 echo "export PATH=\"$HOME/.poetry/bin:$PATH\"" > ~/.bashrc
+export PATH="$HOME/.poetry/bin:$PATH" 
+poetry config virtualenvs.create false
+
 
 curl -s https://deb.nodesource.com/gpgkey/nodesource.gpg.key | apt-key add - \
       && echo 'deb https://deb.nodesource.com/node_14.x buster main' > /etc/apt/sources.list.d/nodesource.list
