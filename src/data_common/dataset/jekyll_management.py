7ac8e73288b4859a7a52bd5d5c1e484f4c768b2c
Alex Parsons <alex.parsons@mysociety.org>
2022-06-07 11:35:32 +0000

    Add download settings processing to jekyll output

--- src/data_common/dataset/jekyll_management.py
+++ src/data_common/dataset/jekyll_management.py
@@ -46,27 +46,40 @@ def collect_jekyll_data():
 
     all_resources = []
     for package in all_packages:
+        package_level_download_options = {}
+        if "download_options" in package:
+            gate_type = package["download_options"].get("gate", "default")
+            survey_ref = package["download_options"].get("survey", "default")
+            header_text = package["download_options"].get("header_text", "default")
+            if gate_type != "default":
+                package_level_download_options["download_gate_type"] = gate_type
+            if survey_ref != "default":
+                package_level_download_options["download_survey"] = survey_ref
+            if header_text != "default":
+                package_level_download_options["download_form_header"] = header_text
+
         package["composite"] = {"xlsx": (package["name"] + "_xlsx").replace("_", "-")}
         for r in package["resources"]:
             r["download_id"] = "_".join([package["name"], r["name"]]).replace("_", "-")
-            all_resources.append(
-                {
-                    "name": r["download_id"],
-                    "package": package["name"],
-                    "title": r["name"],
-                    "filename": r["path"],
-                    "file": f"/data/{package['name']}/{r['path']}",
-                }
-            )
-        all_resources.append(
-            {
-                "name": (package["name"] + "_xlsx").replace("_", "-"),
+            download_data = {
+                "name": r["download_id"],
                 "package": package["name"],
-                "title": package["name"] + "_xlsx",
-                "filename": f"{package['name']}.xlsx",
-                "file": f"/data/{package['name']}/{package['name']}.xlsx",
+                "title": r["name"],
+                "filename": r["path"],
+                "file": f"/data/{package['name']}/{r['path']}",
             }
-        )
+            download_data.update(package_level_download_options)
+            all_resources.append(download_data)
+
+        xlsx_data = {
+                        "name": (package["name"] + "_xlsx").replace("_", "-"),
+                        "package": package["name"],
+                        "title": package["name"] + "_xlsx",
+                        "filename": f"{package['name']}.xlsx",
+                        "file": f"/data/{package['name']}/{package['name']}.xlsx",
+                    }
+        xlsx_data.update(package_level_download_options)
+        all_resources.append(xlsx_data)
 
     render_sources_to_dir(
         all_packages, output_dir=get_settings()["publish_dir"] / "_datasets"
